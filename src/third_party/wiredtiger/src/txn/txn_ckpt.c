bab89640ff29c010e0bb184050a01a631efe8439
Kand Ly <kly@netskope.com>
2020-01-11 01:56:44 +0000

    Add stats to monitor checkpoint slowness and request stalls

--- src/third_party/wiredtiger/src/txn/txn_ckpt.c
+++ src/third_party/wiredtiger/src/txn/txn_ckpt.c
@@ -360,6 +360,7 @@ __wt_checkpoint_get_handles(WT_SESSION_IMPL *session, const char *cfg[])
 	btree->evict_walk_saved = btree->evict_walk_period;
 
 	session->ckpt_handle[session->ckpt_handle_next++] = session->dhandle;
+	WT_STAT_CONN_INCR(session, txn_checkpoint_dhandle_with_ckpt);
 	return (0);
 }
 
@@ -899,9 +900,13 @@ __txn_checkpoint(WT_SESSION_IMPL *session, const char *cfg[])
 	 * Hold the schema lock while starting the transaction and gathering
 	 * handles so the set we get is complete and correct.
 	 */
+	time_start = __wt_clock(session);
 	WT_WITH_SCHEMA_LOCK(session,
 	    ret = __checkpoint_prepare(session, &tracking, cfg));
 	WT_ERR(ret);
+	time_stop = __wt_clock(session);
+	WT_STAT_CONN_INCRV(session, txn_checkpoint_prepare_time,
+		(int64_t)WT_CLOCKDIFF_US(time_stop, time_start));
 
 	WT_ASSERT(session, txn->isolation == WT_ISO_SNAPSHOT);
 
@@ -919,7 +924,11 @@ __txn_checkpoint(WT_SESSION_IMPL *session, const char *cfg[])
 
 	__checkpoint_timing_stress(session);
 
+	time_start = __wt_clock(session);
 	WT_ERR(__checkpoint_apply(session, cfg, __checkpoint_tree_helper));
+	time_stop = __wt_clock(session);
+	WT_STAT_CONN_INCRV(session, txn_checkpoint_tree_helper_time,
+		(int64_t)WT_CLOCKDIFF_US(time_stop, time_start));
 
 	/*
 	 * Clear the dhandle so the visibility check doesn't get confused about
@@ -1571,6 +1580,7 @@ __checkpoint_tree(
 	WT_DATA_HANDLE *dhandle;
 	WT_DECL_RET;
 	WT_LSN ckptlsn;
+	uint64_t time_start, time_stop;
 	bool fake_ckpt, resolve_bm;
 
 	WT_UNUSED(cfg);
@@ -1649,10 +1659,14 @@ __checkpoint_tree(
 	resolve_bm = true;
 
 	/* Flush the file from the cache, creating the checkpoint. */
+	time_start = __wt_clock(session);
 	if (is_checkpoint)
 		WT_ERR(__wt_cache_op(session, WT_SYNC_CHECKPOINT));
 	else
 		WT_ERR(__wt_cache_op(session, WT_SYNC_CLOSE));
+	time_stop = __wt_clock(session);
+	WT_STAT_CONN_INCRV(session, txn_checkpoint_cache_op_time,
+		(int64_t)WT_CLOCKDIFF_US(time_stop, time_start));
 
 	/*
 	 * All blocks being written have been written; set the object's write
@@ -1688,8 +1702,12 @@ fake:	/*
 	    !F_ISSET(&session->txn, WT_TXN_RUNNING))
 		WT_ERR(__wt_checkpoint_sync(session, NULL));
 
+	time_start = __wt_clock(session);
 	WT_ERR(__wt_meta_ckptlist_set(
 	    session, dhandle->name, ckptbase, &ckptlsn));
+	time_stop = __wt_clock(session);
+	WT_STAT_CONN_INCRV(session, txn_checkpoint_meta_ckptlist_set,
+		(int64_t)WT_CLOCKDIFF_US(time_stop, time_start));
 
 	/*
 	 * If we wrote a checkpoint (rather than faking one), we have to resolve
